dabe03e63aa730993270060eafab5d502a88b96b
Angel Bastardo <abastardoing@gmail.com>
2024-01-17 18:09:38 +0100

    Agregar_parametro_TVOC

--- main/transiciones.c
+++ main/transiciones.c
@@ -120,7 +120,7 @@ estado_t trans_estado_calibrado(transicion_t trans) {
 
             char json_buffer[128];
             data_sensores_t *lecturas = trans.dato;
-            sprintf(json_buffer, "{'temperatura': %.3f, 'eCO2': %d}", lecturas->temp_dato, lecturas->CO2_dato);
+            sprintf(json_buffer, "{'temperatura': %.3f, 'eCO2': %d, 'TVOC': %d}", lecturas->temp_dato, lecturas->CO2_dato, lecturas->TVOC_dato);
             thingsboard_telemetry_send(json_buffer);
             ESP_LOGI(TAG, "%s", json_buffer);
 
